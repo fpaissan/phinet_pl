4858cbeca9fd6a3cd394c8b6b467a563e1da787c
Francesco Paissan <me@francescopaissan.it>
2021-11-09 12:47:35 +0000

    Add data augmentation

--- examples/cifar10.py
+++ examples/cifar10.py
@@ -3,6 +3,7 @@ import torchvision.transforms as transforms
 import pytorch_lightning as pl
 import torchvision
 
+from torchvision import transforms
 import torch.nn as nn
 import torchmetrics
 import torch
@@ -10,6 +11,7 @@ import torch
 import cv2
 
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
 from phinet_pl.phinet import PhiNet
 
@@ -28,7 +30,8 @@ class PNCifar10(pl.LightningModule):
             t_zero=6,
             squeeze_excite=True, 
             h_swish=True, 
-            include_top=True
+            include_top=True,
+            p_l = 1
         )
 
         self.accuracy = torchmetrics.classification.accuracy.Accuracy()
@@ -40,21 +43,30 @@ class PNCifar10(pl.LightningModule):
         return nn.CrossEntropyLoss()(out.view(-1, self.num_classes),target)
     
     def configure_optimizers(self):
-        LR = 1e-4
-        optimizer = torch.optim.AdamW(self.parameters(),lr=LR)
-        return optimizer
-
+        LR = 1e-3
+        optimizer = torch.optim.Adam(self.parameters(), lr=LR)
+        
+        sched = CosineAnnealingLR(optimizer, T_max=100)
+        lr_configs = {"scheduler": sched, 
+                      "interval": "epoch", 
+                      "frequency": 1, 
+                      "strict": True,
+                      "name": None}
+        
+        return [optimizer], [lr_configs]
+    
     def training_step(self,batch,batch_idx):
-        x,y = batch
-        img = x.view(-1,3,32,32)
+        x, y = batch
         label = y.view(-1)
+        img = x.view(-1,3,32,32)
+        
         out = self(img)
         loss = self.loss_fn(out,label)
         logits = torch.argmax(out,dim=1)
         accu = self.accuracy(logits, label)
         
-        self.log('train_acc', accu)
-        self.log('train_loss', loss)
+        self.log('train/acc', accu)
+        self.log('train/loss', loss)
 
         return loss       
 
@@ -67,27 +79,50 @@ class PNCifar10(pl.LightningModule):
         logits = torch.argmax(out,dim=1)
         accu = self.accuracy(logits, label)
 
-        self.log('valid_loss', loss)
-        self.log('train_acc_step', accu)
+        self.log('val/loss', loss)
+        self.log('val/acc', accu)
+
+        return loss, accu
+    
+    def test_step(self,batch,batch_idx):
+        x,y = batch
+        img = x.view(-1,3,32,32)
+        label = y.view(-1)
+        out = self(img)
+        loss = self.loss_fn(out,label)
+        logits = torch.argmax(out,dim=1)
+        accu = self.accuracy(logits, label)
+
+        self.log('test/loss', loss)
+        self.log('test/acc', accu)
 
         return loss, accu
 
 
 class Cifar10Dataset(pl.LightningDataModule):
     """Data Module for CIFAR10"""
-    def __init__(self, data_path, batch_size=64):
+    def __init__(self, data_path, batch_size=64, transform: bool = True):
         super().__init__()
         self.batch_size = batch_size
-        self.root_set = torchvision.datasets.CIFAR10(data_path, 
-                                                train=True, 
-                                                download=True, 
-                                                transform=transforms.ToTensor())
+        if transform:
+            self.aug_trans = transforms.Compose([
+                transforms.RandomCrop(32),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.ToTensor()
+            ])
+        else:
+            self.aug_trans = transform.ToTensor()
+        
+        self.root_set = torchvision.datasets.CIFAR10(data_path,
+                                                     train=True,
+                                                     download=True, 
+                                                     transform=self.aug_trans)
 
         self.test_set = torchvision.datasets.CIFAR10(data_path,
-                                                train=False, 
-                                                download=True, 
-                                                transform=transforms.ToTensor())
-    
+                                                     train=False, 
+                                                     download=True, 
+                                                     transform=transforms.ToTensor())
+
     def setup(self, stage=None):
         self.train_set, self.val_set = train_test_split(self.root_set, test_size = 0.3)
 
@@ -96,10 +131,8 @@ class Cifar10Dataset(pl.LightningDataModule):
 
         return DataLoader(self.root_set, batch_size=256, num_workers=16)
 
-    # def val_dataloader(self):
-    #     valid_loader = DataLoader(self.val_set, batch_size=256, num_workers=16)
-
-    #     return valid_loader
-
     def val_dataloader(self):
+        return DataLoader(self.test_set, batch_size=256, num_workers=16)
+    
+    def test_dataloader(self):
         return DataLoader(self.test_set, batch_size=256, num_workers=16)
